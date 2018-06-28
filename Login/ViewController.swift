bf7c8228fcfc2b23cfc8f27ec5f7b835d2c9a01f
MehSwitch <aymehaiDev@protonmail.com>
2018-06-28 16:16:02 -0400

    Added Forgot username and password view controller so when the user forgets either they will be redirected to view controller with a custom msg depending on if they picked "forgot password" or "forgot username"

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    @IBOutlet weak var forgotUsernameButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,28 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func forgotPasswordButton(_ sender: Any) {
+        performSegue(withIdentifier: "forgotUsernameOrPassword", sender: forgotPasswordButton)
+    }
+    
+    @IBAction func forgotUsernameButton(_ sender: Any) {
+        performSegue(withIdentifier: "forgotUsernameOrPassword", sender: forgotUsernameButton)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        guard let sender = sender as? UIButton else {return}
+        if sender == forgotPasswordButton {
+            segue.destination.navigationItem.title = "Forgot Password"
+        }
+        else if sender == forgotUsernameButton {segue.destination.navigationItem.title = "Forgot Username"
+        }
+        else {
+            segue.destination.navigationItem.title = usernameTextField.text
+        }
+        
+    }
 }
 
